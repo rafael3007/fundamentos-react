a3880499c792a94e11c0a97baf12a094f260aaa0
rafael3007 <rafael.brito.1422@gmail.com>
2024-08-27 14:55:11 -0300

    css module

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import Post from "./components/Post";
+import "./styles.css"
+
 
 function App() {
   return (
     <>
-      <Post />
-      <Post />
-      <Post />
+      <Post
+        author="rafael Brito"
+        content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi molestiae nulla nam veniam, ipsum quod natus aut accusamus aliquam consequatur, dolor assumenda magni reprehenderit eum similique omnis obcaecati id velit?"
+      />
+      <Post
+        author="Daniel Brito"
+        content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi molestiae nulla nam veniam, ipsum quod natus aut accusamus aliquam consequatur, dolor assumenda magni reprehenderit eum similique omnis obcaecati id velit?"
+      />
+      <Post
+        author="João Vi"
+        content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi molestiae nulla nam veniam, ipsum quod natus aut accusamus aliquam consequatur, dolor assumenda magni reprehenderit eum similique omnis obcaecati id velit?"
+      />
     </>
   );
 }
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,5 +1,12 @@
-export default function Post() {
-    return(
-        <p>Post</p>
-    )
+import { Header } from "./Header";
+
+/* eslint-disable react/prop-types */
+export default function Post(props) {
+  return (
+    <>
+      <Header />
+      <strong>{props.author}</strong>
+      <p>{props.content}</p>
+    </>
+  );
 }
