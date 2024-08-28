f832ddcf827593483c3aec2dc6b5b52c4ddf83f7
rafael3007 <rafael.brito.1422@gmail.com>
2024-08-28 08:47:47 -0300

    posts

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,10 +1,40 @@
+import styles from "./Post.module.css";
 
-/* eslint-disable react/prop-types */
+ 
 export function Post(props) {
   return (
-    <>
-      <strong>{props.author}</strong>
-      <p>{props.content}</p>
-    </>
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://github.com/rafael3007.png"
+            alt=""
+          />
+          <div className={styles.authorInfo}>
+            <strong>Rafael Brito</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+        <time title="11 de maio às 08:13" dateTime="2022-05-11 08:00:03">
+          publicado há 1hr
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+        </p>
+        <p>
+          👉 <a href="">jane.design/doctorcare</a>
+        </p>
+        <p>
+          <a href="">#novoprojeto #nlw #rocketseat</a>
+        </p>
+      </div>
+    </article>
   );
 }
